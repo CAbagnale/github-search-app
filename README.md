7299eba42607d2bda7d2648525782d541b756cde
Connor Ashpole <connor.ashpole@outlook.com>
2019-07-25 18:11:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# GitHub Search App
+
+# [LIVE APP](https://cabagnale.github.io/github-search-app/index.html)
+
+## About
+
+### Assignment:
+Your team is working on an app that will help recruiters review all of a candidates information. You've been assigned to work on one feature for the app - to display a list of repos belonging to a particular GitHub handle.
+
+## Built With
+
+* HTML
+* CSS
+* JavaScript
+* jQuery
+
+## Authors
+
+* **Connor Ashpole** - [GitHub](https://github.com/CAbagnale)
---
+++ script.js
@@ -0,0 +1,46 @@
+URL = 'https://api.github.com/users/:username/repos';
+
+function getRepos(user) {
+    const searchURL = `https://api.github.com/users/${user}/repos`
+    console.log(searchURL);
+    fetch(searchURL)
+    .then(response => {
+      if (response.length != 0) {
+        return response.json();
+      }
+      throw new Error(response.message);
+    })
+    // .then(response => response.json())
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').html(`<p>Something went wrong: ${err.message}</p><p>The user <strong>${user}</strong> does not exist, or has no repos.</p>`);
+      $('#js-error-message').removeClass('hidden');
+    });
+}
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $('#js-results-display').empty();
+    $('#js-user-display').empty();
+    $('#js-error-message').text('');
+    $('#js-error-message').addClass('hidden');
+    for (let i = 0; i < responseJson.length; i++){
+      $('#js-results-display').append(
+        `<li><h3><a href="${responseJson[i].html_url}">${responseJson[i].name}</a></h3>
+        </li>`
+      )};
+      $('#js-user-display').html(`${responseJson[0].owner.login}'s repos:`);
+    $('#js-results-display').removeClass('hidden');
+    $('.results').removeClass('hidden');
+  };
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchTerm = $('#js-search-user').val();
+        console.log(searchTerm);
+        getRepos(searchTerm);
+      });
+}
+
+$(watchForm);
